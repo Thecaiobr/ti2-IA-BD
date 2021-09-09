f489fbe5f905a01e8d4b5854c7457837a496b84d
Caio Massote <52792957+Thecaiobr@users.noreply.github.com>
2021-09-09 15:19:56 -0300

    Exercicio 1

---
+++ Unidade01/src/somarDoisNumeros.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class somarDoisNumeros {
+	
+		public static Scanner sc = new Scanner(System.in); 
+		
+		public static void main(String arrgs[]) {
+			//declaração de variaveis
+			int num1, num2, soma;
+			
+			//leituras
+			System.out.println("Digite um número: ");
+			num1 = sc.nextInt();
+			System.out.println("Digite outro número: ");
+			num2 = sc.nextInt();
+			
+			//somar
+			soma = num1 + num2;
+			
+			//mostrar na tela
+			System.out.println("Soma: " + soma);
+			
+		}
+	
+}
