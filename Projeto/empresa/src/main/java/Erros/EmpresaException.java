2f9d889bbbd2f30135bfb6ecf5fab44b41892fee
Caio Massote <52792957+Thecaiobr@users.noreply.github.com>
2022-05-05 19:34:26 -0300

    Add files via upload

---
+++ Projeto/empresa/src/main/java/Erros/EmpresaException.java
@@ -0,0 +1,16 @@
+package Erros;
+
+public class EmpresaException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public EmpresaException() {
+		super();
+	}
+
+	public EmpresaException(String message) {
+		super(message);
+	}
+
+	
+}
